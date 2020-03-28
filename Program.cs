06a5473e0a7b03755e2a0c965c7539cc8701ad55
doggiemen <doggiexdoggie@gmail.com>
2020-03-29 03:07:00 +0900

    2nd commit

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ namespace site
 {
     class Program
     {
+        static void Hello(string s) => Console.WriteLine($"Hello {s}");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            Hello("World!");
         }
     }
 }
